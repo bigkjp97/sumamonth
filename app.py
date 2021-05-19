f073ba7ba71c89e4284791bfea794c4987b9836e
bigkjp97 <kunzhoupeng@gmail.com>
2021-05-19 17:43:50 +0800

    short code

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import os
 import re
 
 import yaml
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, make_response, request
 from flask_cors import cross_origin
 
-from utils import Monger
+from utils import Monger, Display
 
 app = Flask(__name__)
 
@@ -34,55 +34,16 @@ def index():
 def api(year, month):
     data = Monger(url, port, dbname, auth, collection, case, utc, hostR, portR, authR, translation[year],
                   translation[month]).index_data()
-
     # Get form content
-
-    def sort_display(arr, req, name):
-        reverse = None
-        if req.form.get(name) == 'DESC':
-            reverse = True
-        elif req.form.get(name) == 'ASC':
-            reverse = False
-        if reverse is not None:
-            arr.sort(key=lambda x: x[name[5:]], reverse=reverse)
-
-    def search_display(arr, cases, attr):
-        # store search cases
-        cTemp = []
-        aTemp = []
-        for i in range(len(cases)):
-            if len(cases[i]) > 0:
-                cTemp.append(cases[i])
-                aTemp.append(attr[i])
-        temp = []
-        resTemp = []
-        # @TODO(code):rubbish
-        if len(cTemp) > 1:
-            for c in range(len(cTemp)):
-                if c == 0:
-                    for a in arr:
-                        if re.search(cTemp[c], a[aTemp[c]]):
-                            temp.append(a)
-                elif len(cTemp) > 1:
-                    for t in temp:
-                        if re.search(cTemp[c], t[aTemp[c]]):
-                            resTemp.append(t)
-            return resTemp
-        else:
-            for a in arr:
-                if re.search(cTemp[0], a[aTemp[0]]):
-                    temp.append(a)
-            return temp
-
-    sort_display(data, request, 'sort_times')
+    D = Display(request)
+    D.sort_display(data, 'sort_times')
     cPage = int(request.form.get('cPage'))
     pSize = int(request.form.get('pSize'))
     res = data[(cPage - 1) * pSize: cPage * pSize]
     if (request.form.get('group') or request.form.get('operators')) is not None:
         if len(request.form.get('group')) > 0 or len(request.form.get('operators')) > 0:
-            search = [request.form.get('group'), request.form.get('operators')]
             attribute = ['group', 'operators']
-            res = search_display(data, search, attribute)[(cPage - 1) * pSize: cPage * pSize]
+            res = D.search_display(data, attribute)[(cPage - 1) * pSize: cPage * pSize]
     apiValue = {
         'status': 'success',
         'totals': len(data),
